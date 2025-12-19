b8c80ffe78cd7c8f958a69696294a46c322a2626
balasubramaniyam-dev <balasubramaniyam.dev@gmail.com>
2025-12-19 10:25:47 +0530

    Add openAPI

--- src/main/java/com/banking/enterprise/core/CoreBankingServiceApplication.java
+++ src/main/java/com/banking/enterprise/core/CoreBankingServiceApplication.java
@@ -1,13 +1,20 @@
 package com.banking.enterprise.core;
 
+import com.corebanking.model.Account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.banking.enterprise",
+        "com.corebanking"
+})
 public class CoreBankingServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoreBankingServiceApplication.class, args);
+        SpringApplication.run(CoreBankingServiceApplication.class, args);
+        Account account = null;
 	}
 
 }
